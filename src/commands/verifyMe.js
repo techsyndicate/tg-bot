3d4e4b6c988dde6369358df88dd31eef6a7110b1
Kunsh Tyagi <k789.tyagi@gmail.com>
2023-04-07 13:04:17 +0530

    added verify command

---
+++ src/commands/verifyMe.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("verify")
+    .setDescription("Sends a dm to the user for verification."),
+  async execute(interaction) {
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("verify_button")
+        .setLabel("Verify Me!")
+        .setStyle(ButtonStyle.Success)
+    );
+    console.log("Made the button");
+    const verMsg = await interaction.member.send({
+      content:
+        "Welcome to the Training Grounds Server! Please verify yourself by clicking the button below:",
+      components: [row],
+    });
+  },
+};
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const {
   GatewayIntentBits,
   Partials,
   EmbedBuilder,
+  Collection,
 } = require("discord.js");
 const client = new Client({
   intents: [
@@ -34,10 +35,26 @@ const eventFiles = fs
 for (const file of eventFiles) {
   const filePath = path.join(eventsPath, file);
   const event = require(filePath);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
+  client.on(event.name, (...args) => event.execute(...args));
+}
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
   } else {
-    client.on(event.name, (...args) => event.execute(...args));
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
   }
 }
 
